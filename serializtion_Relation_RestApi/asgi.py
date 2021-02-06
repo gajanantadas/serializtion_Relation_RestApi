ff84d2d8698ddcb2b2777ba20f2a98007a5704fa
gajanan <gajanantadas3@gmail.com>
2021-02-07 00:57:41 +0530

    Initial commit

---
+++ serializtion_Relation_RestApi/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for serializtion_Relation_RestApi project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'serializtion_Relation_RestApi.settings')
+
+application = get_asgi_application()
---
+++ serializtion_Relation_RestApi/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for serializtion_Relation_RestApi project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'serializtion_Relation_RestApi.settings')
+
+application = get_wsgi_application()
